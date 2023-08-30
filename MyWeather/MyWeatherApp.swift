215f35d19e1612b7567e95b6d947e0d6bd96fcf1
“GodDamnGitHub” <jsczzzx@yahoo.com>
2023-08-30 18:10:30 -0400

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by 张梓欣 on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
