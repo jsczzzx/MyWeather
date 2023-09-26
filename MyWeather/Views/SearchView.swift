d84e5355a49cb569942ddb96b7261cbd915fd8b9
“GodDamnGitHub” <jsczzzx@yahoo.com>
2023-09-26 00:22:05 -0400

    basic search added

--- MyWeather/Views/SearchView.swift
+++ MyWeather/Views/SearchView.swift
@@ -10,18 +10,41 @@ import SwiftUI
 
 struct SearchView: View {
     @State var cityName = ""
+    
+    let names = ["Holly", "Josh", "Rhonda", "Ted","Ada", "Turing", "Ampere", "Renxun","Vega", "RTX", "Honda", "Toyota", "Lovelace", "BMW", "Intel", "Nvidia", "AMD", "RDNA", "Ray", "Gigabyte", "MSI", "Asrock", "Asus"]
+
 
     var body: some View {
-        VStack(alignment: .center) {
+        NavigationStack {
             //Spacer()
-            TextField("Enter city name to search", text: $cityName)
-                .padding()
-            Spacer()
+            List {
+                ForEach(searchResults, id: \.self) { name in
+                    NavigationLink {
+                        Text(name)
+                    } label: {
+                        Text(name)
+                    }
+                }
+            }
+            .navigationTitle("Add New Cities")
+            .padding()
+            
+            //Spacer()
         }
+        .searchable(text: $cityName, prompt: "Enter city name to search")
         .frame(maxWidth: .infinity, alignment: .center)
         .background(.ultraThinMaterial)
         .foregroundColor(.white)
 
+    }
+    
+    var searchResults: [String] {
+        if cityName.isEmpty {
+            return []
+        } else {
+            let res = Array(names.filter { $0.contains(cityName) })
+            return Array(res[0..<min(res.count, 20)])
+        }
     }
     
 }
